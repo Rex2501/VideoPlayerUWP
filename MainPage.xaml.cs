b237b80073f92e3a3801c1e2998544d02154a51c
Yaroslav <kogutyaroslav3947@gmail.com>
2023-08-21 21:26:44 +0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace VideoPlayer {
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page {
+
+        public MainPage() {
+            this.InitializeComponent();
+
+
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
+            base.OnNavigatedTo(e);
+
+            mySwapChainPanel.StartRenderLoop();
+
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e) {
+            mySwapChainPanel.StopRenderLoop();
+        }
+
+    }
+
+}
